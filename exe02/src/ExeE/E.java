3716186f0884d0fddcc1bd3ecdc5248d9ec4286f
RyanCapurso <ryan_capurso@hotmail.com>
2023-05-22 23:10:55 -0300

    Exercicio 2 - E

---
+++ exe02/src/ExeE/E.java
@@ -0,0 +1,39 @@
+package ExeE;
+
+import java.util.Scanner;
+
+public class E {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o código do produto e sua quantidade respectivamente");
+
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+
+        double total;
+        if (codigo == 1) {
+            total = quantidade * 4.0;
+        }
+        else if (codigo == 2) {
+            total = quantidade * 4.5;
+        }
+        else if (codigo == 3) {
+            total = quantidade * 5.0;
+        }
+        else if (codigo == 4) {
+            total = quantidade * 2.0;
+        }
+        else {
+            total = quantidade * 1.5;
+        }
+
+        System.out.printf("Total: R$ %.2f%n", total);
+
+        sc.close();
+
+    }
+
+}
