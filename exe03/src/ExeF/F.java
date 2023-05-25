aa0daaa9a7c250ec7dc4a33fd8063858841c5c48
RyanCapurso <ryan_capurso@hotmail.com>
2023-05-24 23:13:07 -0300

    Exercicio 3 - F

---
+++ exe03/src/ExeF/F.java
@@ -0,0 +1,25 @@
+package ExeF;
+
+import java.util.Scanner;
+
+public class F {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Digite um valor");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+
+    }
+
+}
