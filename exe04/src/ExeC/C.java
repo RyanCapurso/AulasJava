fe168a94aec1537ab19751c79cbb121318db297d
RyanCapurso <ryan_capurso@hotmail.com>
2023-05-24 23:28:17 -0300

    Exercicio 4 - C

---
+++ exe04/src/ExeC/C.java
@@ -0,0 +1,42 @@
+package ExeC;
+
+import java.util.Scanner;
+
+public class C {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("1.Álcool%n2.Gasolina%n3.Diesel%n4.Fim%n");
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        int tipo = sc.nextInt();
+
+        while (tipo != 4) {
+            if (tipo == 1) {
+                alcool = alcool + 1;
+            }
+            else if (tipo == 2) {
+                gasolina = gasolina + 1;
+            }
+            else if (tipo == 3) {
+                diesel = diesel + 1;
+            }
+
+            tipo = sc.nextInt();
+        }
+
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        sc.close();
+
+    }
+
+}
